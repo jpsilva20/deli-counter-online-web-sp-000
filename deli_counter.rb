02d38e8750ca20b55d998abad54d68e5eab0b464
Jennifer Silva <jennifersilva20@gmail.com>
2019-08-27 19:12:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,16 +1,18 @@
 katz_deli = []
 
 def line(katz_deli)
+  line_positions = [];
   if katz_deli.length == 0
     puts "The line is currently empty."
   else 
-    message = "The line is currently:"
-    katz_deli.each_with_index do |name, index|
-      message += "#{index.to_i+1}. #{name}"
+    number = 1 
+    katz_deli.each do |customer| 
+      line_positions << "#{number}. #{customer}"
+      number += 1 
     end 
-    puts "#{message}"
-  end
-end 
+    puts "The line is currently: #{line_positions.join(" ")}"
+    end
+  end 
 
 def take_a_number(katz_deli, name)
   katz_deli << name
